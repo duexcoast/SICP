f442302eecb92b76ba89bfd2aa0a0a66869becb8
duexcoast <conor.ux@gmail.com>
2022-12-13 14:13:12 -0500

    example 1.10

--- chapter-1/1-1.rkt
+++ chapter-1/1-1.rkt
@@ -126,3 +126,59 @@
   (cbrt-iter 1.0 x))
 
 (cuberoot 20)
+
+;;; Exercise 1.9
+
+; (define (+ a b)
+;   (if (= a 0)
+;      b
+;      (inc (+ (dec a) b))))
+
+; --- Substitution Method for (+ 4 5)
+;
+; (inc (+ 3 5)
+; (inc (inc (+ 2 5)))
+; (inc (inc (inc (+ 1 5))))
+; (inc (inc (inc (inc (+ 0 5)))))
+; (inc (inc (inc (inc 5))))
+; (inc (inc (inc 6)))
+; (inc (inc 7))
+; (inc 8)
+; 9
+
+;(define (+ a b)
+;  (if (= a 0)
+;      b
+;      (+ (dec a) (inc b))))
+
+; Iterative process
+; (+ 4 5)
+; (+ (dec 4) (inc 5))
+; (+ (dec 3) (inc 6))
+; (+ (dec 2) (inc 7))
+; (+ (dec 1) (inc 8))
+; (+ (dec 0) (inc 9))
+; 9
+
+;;; Exercise 1.10
+; Ackermann's Function
+
+(define (A x y)
+  (cond ((= y 0) 0)
+        ((= x 0) (* 2 y))
+        ((= y 1) 2)
+        (else (A (- x 1)
+                 (A x (- y 1))))))
+
+; (A 1 10) = 1024
+; (A 2 4) = 65536
+; (A 3 3) = 65536
+
+(define (f n) (A 0 n))
+; (f n) = 2n
+
+(define (g n) (A 1 n))
+; (g n) = 2^n
+
+(define (h n) (A 2 n))
+; (h n) = 2^(h (n - 1))
